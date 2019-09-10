ee7fe8160210bf389fa2d9a65fda778c75d17c14
Angelica Catalan <leicasimile@gmail.com>
2019-09-09 22:49:11 -0400

    Add Bot model

--- src/bots/models.py
+++ src/bots/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Bot(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+        
